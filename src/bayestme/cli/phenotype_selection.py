fda7a72cc67c469b33ff7653e23471e3909a4348
jeffquinn-msk <quinnj2@mskcc.org>
2022-07-08 14:28:04 -0400

    add new modules

---
+++ src/bayestme/cli/phenotype_selection.py
@@ -0,0 +1,52 @@
+import argparse
+import os
+
+from bayestme import phenotype_selection, data
+
+parser = argparse.ArgumentParser(description='Deconvolve data')
+parser.add_argument('--stdata', type=str,
+                    help='Input file, SpatialExpressionDataset in h5 format')
+parser.add_argument('--fold-idx', type=int,
+                    help='Run only this fold index.')
+parser.add_argument('--n-fold', type=int, default=5)
+parser.add_argument('--n-splits', type=int, default=15)
+parser.add_argument('--n-samples', type=int, default=100)
+parser.add_argument('--n-burn', type=int, default=2000)
+parser.add_argument('--n-thin', type=int, default=5)
+parser.add_argument('--n-gene', type=int, default=1000)
+parser.add_argument('--n-components-min', type=int, default=2)
+parser.add_argument('--n-components-max', type=int, default=12)
+parser.add_argument('--lambda-values',
+                    type=float,
+                    action='append',
+                    default=(1, 1e1, 1e2, 1e3, 1e4, 1e5))
+parser.add_argument('--max-ncell', type=int, default=120)
+parser.add_argument('--background-noise', default=False, action='store_true')
+parser.add_argument('--lda-initialization', default=False, action='store_true')
+parser.add_argument('--output-dir', type=str,
+                    help='Output directory')
+
+
+def main():
+    args = parser.parse_args()
+
+    stdata = data.SpatialExpressionDataset.read_h5(args.input)
+
+    result: data.PhenotypeSelectionResult = phenotype_selection.run_phenotype_selection_single_fold(
+        fold_idx=args.fold_idx,
+        stdata=stdata,
+        n_fold=args.n_fold,
+        n_splits=args.n_splits,
+        lams=args.lamda_values,
+        n_components_min=args.n_components_min,
+        n_components_max=args.n_components_max,
+        n_samples=args.n_samples,
+        n_burn=args.n_burn,
+        n_thin=args.n_thin,
+        max_ncell=args.max_ncell,
+        n_gene=args.n_gene,
+        background_noise=args.background_noise,
+        lda_initialization=args.lda_initialization
+    )
+
+    result.save(os.path.join(args.output_dir, 'fold_{}.h5ad'.format(args.fold_idx)))
---
+++ src/bayestme/phenotype_selection.py
@@ -0,0 +1,198 @@
+import logging
+import numpy as np
+
+from sklearn.model_selection import KFold
+from typing import Iterable
+from scipy.stats import multinomial
+
+from bayestme import utils, data
+from bayestme.model_bkg import GraphFusedMultinomial
+
+logger = logging.getLogger(__name__)
+
+
+def get_n_neighbors(stdata: data.SpatialExpressionDataset):
+    """
+    Return a numpy array of size stdata.n_spot_in representing the number of neighbors for each spot.
+    """
+    n_neighbours = np.zeros(stdata.n_spot_in)
+    edges = utils.get_edges(stdata.positions_tissue, layout=stdata.layout.value)
+
+    for i in range(stdata.n_spot_in):
+        n_neighbours[i] = (edges[:, 0] == i).sum() + (edges[:, 1] == i).sum()
+
+    return n_neighbours
+
+
+def create_folds(stdata: data.SpatialExpressionDataset,
+                 n_fold=5,
+                 n_splits=15):
+    kf = KFold(n_splits=n_splits, shuffle=True, random_state=0)
+    n_neighbours = get_n_neighbors(stdata)
+
+    if stdata.layout is data.Layout.HEX:
+        edge_threshold = 5
+    else:
+        edge_threshold = 3
+
+    splits = kf.split(np.arange(stdata.n_spot_in)[n_neighbours > edge_threshold])
+
+    for k in range(n_fold):
+        _, heldout = next(splits)
+        mask = np.array(
+            [i in np.arange(stdata.n_spot_in)[n_neighbours > edge_threshold][heldout] for i in range(stdata.n_spot_in)])
+        yield mask
+
+def plot_folds(stdata: data.SpatialExpressionDataset,
+                 n_fold=5,
+                 n_splits=15):
+    pass
+
+
+def get_phenotype_selection_parameters_for_folds(stdata: data.SpatialExpressionDataset,
+                                                 n_fold: int,
+                                                 n_splits: int,
+                                                 lams: Iterable[int],
+                                                 n_components_min: int,
+                                                 n_components_max: int):
+    for lam in lams:
+        for n_components in range(n_components_min, n_components_max + 1):
+            for fold_number, mask in enumerate(create_folds(stdata, n_fold, n_splits)):
+                yield lam, n_components, mask, fold_number
+
+
+def sample_graph_fused_multinomial(
+        train: np.ndarray,
+        test: np.ndarray,
+        n_components,
+        edges: np.ndarray,
+        n_gene: int,
+        lam_psi: float,
+        background_noise: bool,
+        lda_initialization: bool,
+        mask: np.ndarray,
+        n_max: int,
+        n_samples: int,
+        n_thin: int,
+        n_burn: int):
+    n_nodes = train.shape[0]
+
+    heldout_spots = np.argwhere(mask).flatten()
+    train_spots = np.argwhere(~mask).flatten()
+    if len(heldout_spots) == 0:
+        mask = None
+
+    graph_fused_multinomial = GraphFusedMultinomial(
+        n_components=n_components,
+        edges=edges,
+        observations=train,
+        n_gene=n_gene,
+        lam_psi=lam_psi,
+        background_noise=background_noise,
+        lda_initialization=lda_initialization,
+        mask=mask,
+        n_max=n_max)
+
+    cell_prob_trace = np.zeros((n_samples, n_nodes, n_components + 1))
+    cell_num_trace = np.zeros((n_samples, n_nodes, n_components + 1))
+    expression_trace = np.zeros((n_samples, n_components, n_gene))
+    beta_trace = np.zeros((n_samples, n_components))
+    loglhtest_trace = np.zeros(n_samples)
+    loglhtrain_trace = np.zeros(n_samples)
+
+    for step in range(n_samples * n_thin + n_burn):
+        if step % 10 == 0:
+            logger.info(f'Step {step}')
+        # perform Gibbs sampling
+        graph_fused_multinomial.sample(train)
+        # save the trace of GFMM parameters
+        if step >= n_burn and (step - n_burn) % n_thin == 0:
+            idx = (step - n_burn) // n_thin
+            cell_prob_trace[idx] = graph_fused_multinomial.probs
+            expression_trace[idx] = graph_fused_multinomial.phi
+            beta_trace[idx] = graph_fused_multinomial.beta
+            cell_num_trace[idx] = graph_fused_multinomial.cell_num
+            rates = (graph_fused_multinomial.probs[:, 1:][:, :, None] * (
+                    graph_fused_multinomial.beta[:, None] * graph_fused_multinomial.phi)[None])
+            nb_probs = rates.sum(axis=1) / rates.sum(axis=(1, 2))[:, None]
+            loglhtest_trace[idx] = np.array(
+                [multinomial.logpmf(test[i], test[i].sum(), nb_probs[i]) for i in heldout_spots]).sum()
+            loglhtrain_trace[idx] = np.array(
+                [multinomial.logpmf(train[i], train[i].sum(), nb_probs[i]) for i in train_spots]).sum()
+            logger.info('{}, {}'.format(loglhtrain_trace[idx], loglhtest_trace[idx]))
+
+    return (
+        cell_prob_trace,
+        cell_num_trace,
+        expression_trace,
+        beta_trace,
+        loglhtest_trace,
+        loglhtrain_trace
+    )
+
+
+def run_phenotype_selection_single_fold(
+        fold_idx: int,
+        stdata: data.SpatialExpressionDataset,
+        n_fold: int,
+        n_splits: int,
+        lams: Iterable[int],
+        n_components_min: int,
+        n_components_max: int,
+        n_samples: int,
+        n_burn: int,
+        n_thin: int,
+        max_ncell: int,
+        n_gene: int,
+        background_noise: bool,
+        lda_initialization: bool) -> data.PhenotypeSelectionResult:
+    params = [_ for _ in get_phenotype_selection_parameters_for_folds(
+        stdata,
+        n_fold,
+        n_splits,
+        lams,
+        n_components_min,
+        n_components_max)]
+
+    lam, n_components, mask, fold_number = params[fold_idx]
+
+    train = stdata.reads.copy()
+    test = stdata.reads.copy()
+    train[mask] = 0
+    test[~mask] = 0
+
+    n_gene = min(n_gene, stdata.n_gene)
+
+    (
+        cell_prob_trace,
+        cell_num_trace,
+        expression_trace,
+        beta_trace,
+        loglhtest_trace,
+        loglhtrain_trace
+    ) = sample_graph_fused_multinomial(
+        train=train,
+        test=test,
+        n_components=n_components,
+        edges=stdata.edges,
+        n_gene=n_gene,
+        lam_psi=lam,
+        background_noise=background_noise,
+        lda_initialization=lda_initialization,
+        mask=mask,
+        n_max=max_ncell,
+        n_samples=n_samples,
+        n_thin=n_thin,
+        n_burn=n_burn)
+
+    return data.PhenotypeSelectionResult(
+        mask=mask,
+        cell_prob_trace=cell_prob_trace,
+        expression_trace=expression_trace,
+        beta_trace=beta_trace,
+        cell_num_trace=cell_num_trace,
+        log_lh_train_trace=loglhtrain_trace,
+        log_lh_test_trace=loglhtest_trace,
+        lam=lam,
+        n_components=n_components,
+        fold_number=fold_number)
