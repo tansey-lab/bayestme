6bf11d9acf7f472d71e427a675a748612084a7ba
jeffquinn-msk <quinnj2@mskcc.org>
2022-08-04 13:02:19 -0400

    integration test pass

--- src/bayestme/cli/deconvolve.py
+++ src/bayestme/cli/deconvolve.py
@@ -15,7 +15,8 @@ parser.add_argument('--output', type=str,
 parser.add_argument('--n-gene', type=int,
                     help='number of genes')
 parser.add_argument('--n-components', type=int,
-                    help='Number of cell types, expected to be determined from cross validation.')
+                    help='Number of cell types, expected to be determined from cross validation.',
+                    default=None)
 parser.add_argument('--lam2', type=int,
                     help='Smoothness parameter, this tuning parameter expected to be determined'
                          'from cross validation.')
@@ -39,6 +40,10 @@ parser.add_argument('--lda',
                     help='Turn LDA Initialization on',
                     action='store_true',
                     default=False)
+parser.add_argument('--expression-truth',
+                    help='Use expression ground truth from seurat companion scRNA fine mapping workflow',
+                    type=str,
+                    default=None)
 
 
 def main():
@@ -46,18 +51,29 @@ def main():
 
     dataset: data.SpatialExpressionDataset = data.SpatialExpressionDataset.read_h5(args.input)
 
+    if args.expression_truth:
+        expression_truth = deconvolution.load_expression_truth(dataset, args.expression_truth)
+        n_components = expression_truth.shape[0]
+    else:
+        expression_truth = None
+        n_components = args.n_components
+
+    if n_components is None:
+        raise RuntimeError('--n-components not explicitly provided, and no expression truth provided.')
+
     results: data.DeconvolutionResult = deconvolution.deconvolve(
         reads=dataset.reads,
         edges=dataset.edges,
         n_gene=args.n_gene,
-        n_components=args.n_components,
+        n_components=n_components,
         lam2=args.lam2,
         n_samples=args.n_samples,
         n_burnin=args.n_burnin,
         n_thin=args.n_thin,
         random_seed=args.random_seed,
         bkg=args.bkg,
-        lda=args.lda
+        lda=args.lda,
+        expression_truth=expression_truth
     )
 
     results.save(args.output)
--- src/bayestme/cli/deconvolve_test.py
+++ src/bayestme/cli/deconvolve_test.py
@@ -10,7 +10,7 @@ from bayestme.cli import deconvolve
 from bayestme.data_test import generate_toy_stdataset
 
 
-def test_filter_genes():
+def test_deconvolve():
     dataset = generate_toy_stdataset()
     tmpdir = tempfile.mkdtemp()
 
@@ -61,7 +61,69 @@ def test_filter_genes():
                     n_thin=2,
                     random_seed=0,
                     bkg=False,
-                    lda=False
+                    lda=False,
+                    expression_truth=None
                 )
+    finally:
+        shutil.rmtree(tmpdir)
+
+
+def test_deconvolve_with_expression_truth():
+    dataset = generate_toy_stdataset()
+    tmpdir = tempfile.mkdtemp()
+
+    input_path = os.path.join(tmpdir, 'data.h5')
+    output_path = os.path.join(tmpdir, 'deconvolve.h5')
+
+    deconvolve_rv = data.DeconvolutionResult(
+        cell_prob_trace=np.zeros((2, 2)),
+        expression_trace=np.zeros((2, 2)),
+        beta_trace=np.zeros((2, 2)),
+        cell_num_trace=np.zeros((2, 2)),
+        reads_trace=np.zeros((2, 2)),
+        lam2=1000,
+        n_components=4
+    )
+
+    command_line_arguments = [
+        'deconvolve',
+        '--input', input_path,
+        '--output', output_path,
+        '--n-gene', '1000',
+        '--lam2', '1000',
+        '--n-samples', '100',
+        '--n-burnin', '500',
+        '--n-thin', '2',
+        '--expression-truth', 'xxx'
+    ]
+
+    try:
+        dataset.save(input_path)
+
+        with mock.patch('sys.argv', command_line_arguments):
+            with mock.patch('bayestme.deconvolution.deconvolve') as deconvolve_mock:
+                with mock.patch('bayestme.deconvolution.load_expression_truth') as load_expression_truth_mock:
+                    expression_truth = np.zeros((9, 10))
+                    load_expression_truth_mock.return_value = expression_truth
+                    deconvolve_mock.return_value = deconvolve_rv
+
+                    deconvolve.main()
+
+                    data.DeconvolutionResult.read_h5(output_path)
+
+                    deconvolve_mock.assert_called_once_with(
+                        reads=mock.ANY,
+                        edges=mock.ANY,
+                        n_gene=1000,
+                        n_components=9,
+                        lam2=1000,
+                        n_samples=100,
+                        n_burnin=500,
+                        n_thin=2,
+                        random_seed=0,
+                        bkg=False,
+                        lda=False,
+                        expression_truth=mock.ANY
+                    )
     finally:
         shutil.rmtree(tmpdir)
