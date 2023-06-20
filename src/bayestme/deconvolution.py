f32412efa9fc8bafa1519780609eac68247dfea2
jeffquinn-msk <quinnj2@mskcc.org>
2023-06-20 14:26:32 -0400

    need to refactor svi code a little bit

--- src/bayestme/deconvolution.py
+++ src/bayestme/deconvolution.py
@@ -3,6 +3,9 @@ from numpy.random import Generator
 from typing import Optional
 from bayestme.common import InferenceType
 
+import bayestme.mcmc.deconvolution
+import bayestme.svi.deconvolution
+
 
 def sample_from_posterior(
     data: SpatialExpressionDataset,
@@ -10,7 +13,23 @@ def sample_from_posterior(
     spatial_smoothing_parameter=None,
     n_samples=100,
     expression_truth=None,
-    InferenceType=InferenceType.MCMC,
+    inference_type=InferenceType.MCMC,
     rng: Optional[Generator] = None,
 ):
-    pass
+    if inference_type == InferenceType.MCMC:
+        bayestme.mcmc.deconvolution.deconvolve(
+            reads=data.reads,
+            edges=data.edges,
+            n_samples=n_samples,
+            n_burnin=1000,
+            n_thin=5,
+            n_gene=data.n_gene,
+            n_components=n_components,
+            lam2=spatial_smoothing_parameter,
+            expression_truth=expression_truth,
+            rng=rng,
+        )
+    elif inference_type == InferenceType.SVI:
+        pass
+    else:
+        raise ValueError()
